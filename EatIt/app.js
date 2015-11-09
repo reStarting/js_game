09e459d67289ab12c7f4c79420899d2974f51914
reStarting <172603678@qq.com>
2015-11-09 16:45:52 +0800

    build framework

---
+++ EatIt/app.js
@@ -0,0 +1,9 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game')
+
+// var Welcome = require('./scene/welcome.js')(game);
+
+// game.state.add('welcome', Welcome);
+
+// game.state.start('welcome');
+
+
