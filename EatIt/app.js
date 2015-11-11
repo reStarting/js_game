7b0cdaa557f7b92f10b38eca2b1b5cbc7a5d87a8
reStarting <172603678@qq.com>
2015-11-11 09:22:08 +0800

    del EatIt

--- EatIt/app.js
+++
@@ -1,9 +0,0 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game')
-
-// var Welcome = require('./scene/welcome.js')(game);
-
-// game.state.add('welcome', Welcome);
-
-// game.state.start('welcome');
-
-
