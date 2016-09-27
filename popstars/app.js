e67c9377b7d3c1d7ab347dc9d24a79e4b781af96
reStarting <172603678@qq.com>
2016-09-27 19:20:08 +0800

    mod

--- popstars/app.js
+++ popstars/app.js
@@ -1,14 +1,10 @@
 var game = new Phaser.Game(480, 800, Phaser.AUTO, 'game')
 
-var Welcome = require('./scene/welcome.js')(game);
+var Welcome = require('./scene/welcome.js')(game)
 
-game.state.add('welcome', Welcome);
+game.state.add('welcome', Welcome)
 
+game.state.start('welcome')
 
-game.state.start('welcome');
-
-
-function app()
-{
-
+function app () {
 }
